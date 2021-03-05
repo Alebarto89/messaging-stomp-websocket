a3e219dfd1eb35eacd0a89ae96095c1a399e63f6
alessio <alessiobartoli39@gmail.com>
2021-03-05 11:47:33 +0100

    Finito

--- src/main/java/com/example/messagingstompwebsocket/MessagingStompWebsocketApplication.java
+++ src/main/java/com/example/messagingstompwebsocket/MessagingStompWebsocketApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MessagingStompWebsocketApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(MessagingStompWebsocketApplication.class, args);
 	}
 
